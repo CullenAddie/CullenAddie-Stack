19665dbecea4c6ac5ea9d1186b379b6d404d5fec
CullenAddie <cullenaddie1@gmail.com>
2023-03-03 21:18:30 +0000

    cullen

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 /**
  * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
@@ -13,22 +14,25 @@ import java.util.Deque;
 public class Lab {
     /**
      * Implement and return some class that implements Deque, such as ArrayDeque, which contains all the functionality
-     * of a Stack. This challenge will not be using the Stack class, because the Stack class is known to have a shoddy
+     * of a Stack. This (challenge will not be using the Stack class, because the Stack class is known to have a shoddy
      * implementation as it hasn't been changed since the very earliest version of Java. Deques were made to exhibit
      * the exact same behavior and are generally better.
      *
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
     public Deque<String> createNewStack(){
-        return null;
+        ArrayDeque<String> dq = new ArrayDeque<String>();
+        return (dq);
     }
+
+    
 
     /**
      * @param stack A Deque of Strings.
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,6 +42,7 @@ public class Lab {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.push(item);
     }
 
     /**
@@ -47,7 +52,8 @@ public class Lab {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+        stack.getFirst();
+        return stack.pop();
     }
     /**
      * Peek the value at the top of a deque.
@@ -57,6 +63,7 @@ public class Lab {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        stack.peek();
+        return stack.peek();
     }
 }
